56013cab4bc913dd85168b91607b51c078e9beba
Sheiryrozy01 <81344150+Sheiryrozy01@users.noreply.github.com>
2021-03-27 23:13:27 +0800

    Add files via upload

---
+++ addingInput.py
@@ -0,0 +1,11 @@
+total = 0;
+expense = [];
+expense_count = int(input("Enter # of expense : "));
+for i in range(expense_count):
+    expense.append(float(input("Enter an expense : RM ")));
+
+total = sum(expense);
+formula = "";
+for each in expense:
+    formula +=str(each) +" + ";
+print("\nYou spend "+formula+"0 = RM "+str(total));
